087ade14ebf47f4fb3c55afd5384e3c18a5cd50e
pulgamecanica <pulgamecanica11@gmail.com>
2023-08-01 19:44:02 +0200

    Create Sessions controller and helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :messages
   resources :room_players
   resources :players
   resources :game_rooms do
